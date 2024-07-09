c606b4bdca5b473855dd617864467aa3dce8c1d4
Raf-Taufiqurrahman <raf.taufiqurrahman@gmail.com>
2024-07-09 23:58:50 +0700

    feat(add) : add utils to handle roles & permissions

---
+++ resources/js/Utils/Permissions.jsx
@@ -0,0 +1,32 @@
+import { usePage } from "@inertiajs/react";
+
+export default function hasAnyPermission(permissions) {
+
+    // destruct auth from usepage props
+    const { auth } = usePage().props
+
+    // get all permissions from props auth
+    let allPermissions = auth.permissions;
+
+    // get super-admin role from props auth
+    let superAdmin = auth.super;
+
+    // do it when super admin true
+    if(superAdmin)
+        // return true;
+        return true;
+
+    // define has permission is false
+    let hasPermission = false;
+
+    // loop permissions
+    permissions.forEach(function(item){
+        // do it if permission is match with key
+        if(allPermissions[item])
+            // assign hasPermission to true
+            hasPermission = true;
+    });
+
+    // return has permissions
+    return hasPermission;
+}
